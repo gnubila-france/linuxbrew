f3548dbc06a49d5286d8835ad25a52c8045071a3
Jerome Revillard <jrevillard@maatg.fr>
2014-08-25 16:53:01 +0200

    Try to fix the X11 detection also faking the X version for the moment

--- Library/Homebrew/os/x11.rb
+++ Library/Homebrew/os/x11.rb
@@ -17,7 +17,13 @@ module OS
     def detect_version
       begin
         version_string = `xterm -version`
-        version_string.match /\((\d+)\)/
+        version_string = version_string.match /\((\d+)\)/
+	if version_string.nil?
+          "0.0.0"
+        else
+          #return a number as we do not know the real version number
+          "2.10"
+        end
       rescue Errno::ENOENT
       end
     end
