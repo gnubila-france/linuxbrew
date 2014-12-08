8d9e0683d00a995340cef7fbb4a7330fa89b276a
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-12-08 00:10:06 +0000

    delete parent too

--- Library/Formula/gcc.rb
+++ Library/Formula/gcc.rb
@@ -217,7 +217,9 @@ class Gcc < Formula
     end
     
     if OS.linux?
+      p = link.parent
       link.delete
+      p.delete
       crts = Pathname.new "#{lib}/gcc/x86_64-unknown-linux-gnu/#{version}"
       Formula['glibc'].lib.children.select {|p| p.basename.to_s =~ /^crt.\.o$/ }.collect {|p| p.relative_path_from crts}.each do |p|
         crts.install_symlink p 
